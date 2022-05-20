be413b3c34fcc35cb5bdd587205644efc0442fd8
Balint Erdi <balint@balinterdi.com>
2022-05-20 15:59:44 +0200

    Comms work between content- and background script

---
+++ background.js
@@ -0,0 +1,25 @@
+console.log('background script');
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {greeting: "Hi from background script"}
+    ).then(response => {
+      console.log("Message from the content script:");
+      console.log(response.response);
+    }).catch(onError);
+  }
+}
+
+browser.browserAction.onClicked.addListener(() => {
+  console.log('Button clicked');
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs).catch(onError)
+});
---
+++ set-random-bk.js
@@ -0,0 +1,5 @@
+browser.runtime.onMessage.addListener(request => {
+  console.log("Message from the background script:");
+  console.log(request.greeting);
+  return Promise.resolve({response: "Hi from content script"});
+});
