79aa77a7fccb7d9951ada40312d86f2cfffead61
Balint Erdi <balint@balinterdi.com>
2022-05-27 16:55:47 +0200

    Loading a fen and starting from position works

--- background.js
+++ background.js
@@ -2,14 +2,21 @@ function onError(error) {
   console.error(`Error: ${error}`);
 }
 
+const sendMessage = browser.tabs.sendMessage;
+
 function sendSetUpBoard(tabs) {
-  console.log('sendSetupBoard');
   for (let tab of tabs) {
     browser.tabs
       .sendMessage(tab.id, { messageId: "set-up-board" })
       .then((response) => {
-        console.log("Message from the content script:");
-        console.log(response.response);
+        if (response.response === 'on-play-modal') {
+          setTimeout(() => {
+            sendMessage(tab.id, { messageId: "load-fen" })
+              .then((response) => {
+                console.log('Response for on-play-modal', response.response);
+              });
+          }, 250);
+        }
       })
       .catch(onError);
   }
--- set-random-bk.js
+++ set-random-bk.js
@@ -6,77 +6,45 @@ function findActionButtonWithText(text) {
   });
 }
 
-function clickPlayAsRandom() {
-  console.log('clickPlayAsRandom');
-  let randomSideButton = document.querySelector('.color-submits [title="Random side"]');
-  randomSideButton.click();
+function logElements() {
+  console.log(document.querySelector("#modal-wrap"));
+  console.log(document.querySelector("cg-board"));
+  console.log(document.querySelector(".setup-content"));
+  console.log(document.querySelector('.color-submits [title="White"]'));
 }
 
-function logElements() {
-  console.log(document.querySelector('#modal-wrap'));
-  console.log(document.querySelector('cg-board'));
-  console.log(document.querySelector('.setup-content'));
-  console.log(document.querySelector('.color-submits [title="Random side"]'));
+function startPlayWithComputer(resolve) {
+  let continueButton = findActionButtonWithText("continue from here");
+  continueButton.click();
+
+  let playWithComputer = document.querySelector("#modal-wrap .button");
+  playWithComputer.click();
+
+  resolve({ response: 'on-play-modal' });
+}
+
+function loadFen(resolve) {
+  let fenInput = document.querySelector("#fen-input");
+  fenInput.value = "4k3/8/8/3B4/8/3K1N2/8/8 w - - 0 1";
+  let event = new InputEvent('input');
+  fenInput.dispatchEvent(event);
+
+  let playAsWhiteButton = document.querySelector('.color-submits [title="White"]');
+  playAsWhiteButton.click();
+  resolve({ response: 'play-as-white' });
 }
 
 browser.runtime.onMessage.addListener((request) => {
+  // One needs to be on Board Editor /editor for the following to work
+  // TODO: The first step should be to go to /editor
   return new Promise((resolve, reject) => {
-    if (request.messageId === "set-up-board") {
-      console.log('a');
-      /*
-      let clearBoardButton = findActionButtonWithText("clear board");
-      if (!clearBoardButton) {
-        return Promise.reject({ response: "Couldn't find Clear board button" });
-      }
-      clearBoardButton.click();
-      */
-      setTimeout(() => {
-        console.log('setTimeout 1');
-      }, 10);
-      let continueButton = findActionButtonWithText('continue from here');
-      continueButton.click();
+    let { messageId } = request;
+    if (messageId === "set-up-board") {
+      startPlayWithComputer(resolve);
+    }
 
-      let playWithComputer = document.querySelector('#modal-wrap .button');
-      playWithComputer.click();
-
-      setTimeout(() => {
-        console.log('setTimeout 2');
-      }, 10);
-  
-      console.log('2');
-      logElements();
-      setTimeout(() => {
-        console.log('setTimeout 3');
-        logElements();
-        resolve({ response: 'From inside setTimeout 3'});
-        /*
-        console.log('3');
-        logElements();
-        let randomSideButton = document.querySelector('.color-submits [title="Random side"]');
-        if (!randomSideButton) {
-          console.log('3a');
-          reject("Didn't find Random side button");
-        }
-        console.log('3b');
-        randomSideButton.click();
-        resolve({ response: "Could click Random side button"});
-        */
-      }, 50);
-
-      // console.log('2');
-      // resolve({ response: 'End'});
-  
-    //  let fenInput = document.querySelector('#fen-input');
-    //  fenInput.value = '4k3/8/8/3B4/8/3K1N2/8/8 w - - 0 1';
-    //  fenInput.blur();
-  
-      // let whiteKnight = document.querySelector('.spare-white [data-role="knight"]');
-      // whiteKnight.parentElement.parentElement.click();
-  
-      // document.querySelector('.copyables .copyable').value = '4k3/8/8/3B4/8/3K1N2/8/8 w - - 0 1';
-  
-      // return Promise.resolve({ response: "Clicked Clear board button" });
+    if (messageId === "load-fen") {
+      loadFen(resolve);
     }
   });
 });
-
